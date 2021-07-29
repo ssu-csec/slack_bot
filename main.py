d1c70898f180a2fd8b5a3eb08694de338481f9c7
kordood <gigacms@gmail.com>
2021-07-29 09:58:48 +0000

    Delete condition only libert_dm for debugging

--- main.py
+++ main.py
@@ -15,13 +15,12 @@ if __name__ == '__main__':
     path = FilePath
     bot_list = []
     for key, webhook in Url.webhooks.items():
-        if key == "liberty_dm":
-            nb = [NoticeBot(Crawler.crawl_grad_notice, webhook, path.crawler_data["grad_info"]),
-                    NoticeBot(Crawler.crawl_ssu_notice, webhook, path.crawler_data["ssu_info"], seek_time=7200),
-                    NoticeBot(Crawler.crawl_sw_grad_notice, webhook, path.crawler_data["sw_grad_info"]),
-                    NoticeBot(Crawler.crawl_sw_dept_notice, webhook, path.crawler_data["sw_dept_info"]),
-                    NoticeBot(Crawler.crawl_sw_job_notice, webhook, path.crawler_data["sw_job_info"])]
-            bot_list += nb
+        nb = [NoticeBot(Crawler.crawl_grad_notice, webhook, path.crawler_data["grad_info"]),
+                NoticeBot(Crawler.crawl_ssu_notice, webhook, path.crawler_data["ssu_info"], seek_time=7200),
+                NoticeBot(Crawler.crawl_sw_grad_notice, webhook, path.crawler_data["sw_grad_info"]),
+                NoticeBot(Crawler.crawl_sw_dept_notice, webhook, path.crawler_data["sw_dept_info"]),
+                NoticeBot(Crawler.crawl_sw_job_notice, webhook, path.crawler_data["sw_job_info"])]
+        bot_list += nb
 
     for bot in bot_list:
         bot.daemon = True
