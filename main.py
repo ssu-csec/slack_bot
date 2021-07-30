78fb17754726e0e3474e2334bfb80880a4f6caae
kordood <gigacms@gmail.com>
2021-07-30 06:34:34 +0000

    Seperate webhook kinds for grad, test

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ if __name__ == '__main__':
     path = FilePath
     bot_list = []
     for key, webhook in Url.webhooks.items():
-        nb = [NoticeBot(Crawler.crawl_grad_notice, webhook, path.crawler_data["grad_info"]),
-                NoticeBot(Crawler.crawl_ssu_notice, webhook, path.crawler_data["ssu_info"], seek_time=7200),
-                NoticeBot(Crawler.crawl_sw_grad_notice, webhook, path.crawler_data["sw_grad_info"]),
+        if "grad" in key:
+            nb = [NoticeBot(Crawler.crawl_grad_notice, webhook, path.crawler_data["grad_info"]),
+                NoticeBot(Crawler.crawl_sw_grad_notice, webhook, path.crawler_data["sw_grad_info"])]
+        elif "test" in key:
+            continue
+        else:
+            nb = [NoticeBot(Crawler.crawl_ssu_notice, webhook, path.crawler_data["ssu_info"], seek_time=7200),
                 NoticeBot(Crawler.crawl_sw_dept_notice, webhook, path.crawler_data["sw_dept_info"]),
                 NoticeBot(Crawler.crawl_sw_job_notice, webhook, path.crawler_data["sw_job_info"])]
         bot_list += nb
